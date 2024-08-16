2c845236d94fdeb6443b4422a420ce0af5855b97
Axcent <97802272+Axcent-ape@users.noreply.github.com>
2024-08-16 21:17:17 +0300

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,32 @@
+# api id, hash
+API_ID = 1488
+API_HASH = 'abcde1488'
+
+
+DELAYS = {
+    'ACCOUNT': [5, 15],  # delay between connections to accounts (the more accounts, the longer the delay)
+    'TASK': [5, 10],     # delay after completed the task
+}
+
+PROXY = {
+    "USE_PROXY_FROM_FILE": False,  # True - if use proxy from file, False - if use proxy from accounts.json
+    "PROXY_PATH": "data/proxy.txt",  # path to file proxy
+    "TYPE": {
+        "TG": "socks5",  # proxy type for tg client. "socks4", "socks5" and "http" are supported
+        "REQUESTS": "socks5"  # proxy type for requests. "http" for https and http proxys, "socks5" for socks5 proxy.
+        }
+}
+# session folder (do not change)
+WORKDIR = "sessions/"
+
+# timeout in seconds for checking accounts on valid
+TIMEOUT = 30
+
+SOFT_INFO = f"""{"BOOL BOT".center(40)}
+Soft for https://t.me/boolfamily_Bot
+{"Functional:".center(40)}
+Set username (if need); register accounts in web app; complete tasks; 
+
+The soft also collects statistics on accounts and uses proxies from {f"the {PROXY['PROXY_PATH']} file" if PROXY['USE_PROXY_FROM_FILE'] else "the accounts.json file"}
+To buy this soft with the option to set your referral link write me: https://t.me/Axcent_ape
+"""
